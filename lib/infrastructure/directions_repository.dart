6a7a2fc7c03f7ebbfae93efd5b42ceded4f0cc51
ferdinand <ferdinandmwal@gmail.com>
2021-04-29 12:48:14 +0300

    dio

--- lib/infrastructure/directions_repository.dart
+++ lib/infrastructure/directions_repository.dart
@@ -1,3 +1,8 @@
+import 'package:dio/dio.dart';
+
 class DirectionsRepository {
-  
+  static const String _baseUrl =
+      'https://maps.googleapis.com/maps/api/directions/json?';
+
+  final Dio _dio;
 }
