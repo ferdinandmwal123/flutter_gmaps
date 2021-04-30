0374b69ded82f858411e296ff3a14ef5d0139c1d
ferdinand <ferdinandmwal@gmail.com>
2021-04-30 11:21:48 +0300

    class parameters

---
+++ lib/infrastructure/directions_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class Directions {
+  final LatLngBounds bounds;
+  final List<PointLatLng> polylinePoints;
+  final String totalDistance;
+  final String totalDuration;
+}
